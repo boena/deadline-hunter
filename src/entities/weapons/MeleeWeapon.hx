746c5635581aacd328dc770fa13018d5bd6a2d74
Bjorn Lennartsson <bjorn@isotop.se>
2012-12-07 14:29:59 +0100

    Working weapon

--- src/entities/weapons/MeleeWeapon.hx
+++ src/entities/weapons/MeleeWeapon.hx
@@ -5,25 +5,53 @@ class MeleeWeapon extends Weapon {
 	
 	private var _player : Player;
 	private var _hittingLength : Int;
-	
+	private var targetX : Int;
+	private var inMotion : Bool;
+	private var reverseMotion : Bool;
+
 	public override function update()
 	{
 		super.update();
-		
+				
 		y = _player.y + _player.height / 2;
 
-		if(_player.facingLeft)
+		if( targetX < _hittingLength && !reverseMotion )
+		{	
+			targetX++;
+		}
+		else
 		{
-			x = _player.x - width + 6;
+			reverseMotion = true;
+			targetX--;
+		}
+
+		if(_player.facingLeft)
+		{	
+			x = _player.x - width + 8;
+			x -= targetX;
 		}
 		else
 		{
 			x = _player.x + _player.width - 6;
+			x += targetX;
 		}
+
+		if(targetX == 0)
+		{
+			inMotion = false;
+			graphic.visible = false;
+		}
+		
 	}
 
 	public override function use()
 	{
-		graphic.visible = true;
+		if( !inMotion ) 
+		{
+			inMotion = true;
+			reverseMotion = false;
+			targetX = 0;
+			graphic.visible = true;
+		}
 	}
 }
